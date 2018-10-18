3d23cb26a1717ec5ce2ec6a2caea038289324de6
Han Trinh <hantn16@gmail.com>
2018-10-18 10:43:14 +0700

    Add project files.

---
+++ QLNS.GUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace DETAI
+{
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmcauhinh());    
+        }
+    }
+}
